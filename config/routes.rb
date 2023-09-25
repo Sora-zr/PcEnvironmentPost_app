90da6d45712702a057cccf682c23523d9c1f2ef3
sora <h.sora1105@gmail.com>
2023-09-26 01:15:42 +0900

    Add: deviseで認証機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#top'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
