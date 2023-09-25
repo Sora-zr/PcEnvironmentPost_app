8a91f960b5ddb4d9c00d17799a42d7c4b392e2d3
sora <h.sora1105@gmail.com>
2023-09-25 17:59:28 +0900

    Set: アプリケーションのタイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module PcEnvironmentPostApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
