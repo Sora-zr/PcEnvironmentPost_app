93460da05c0e0f99de302f560e80603e6a7a07e8
sora <h.sora1105@gmail.com>
2023-10-01 11:24:53 +0900

    Add: Userモデルのテスト設定など

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    user_name { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー登録' do
+    it '入力値が正常であれば問題なくユーザー登録できること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
