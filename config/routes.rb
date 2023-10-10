5f08468272c4a6d369d84ec67367b6e2b67b24f0
sora <h.sora1105@gmail.com>
2023-10-10 23:13:00 +0900

    Fix: ログイン前のホームページのデザインを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get 'users/sign_out', to: 'devise/sessions#destroy'
-    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 
