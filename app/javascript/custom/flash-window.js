e5589376446ee1651397985a27a3f8174de9c2bd
sora <h.sora1105@gmail.com>
2023-10-19 20:58:37 +0900

    Fix: ログインページとユーザー登録ページのスタイルを変更

--- app/javascript/custom/flash-window.js
+++ app/javascript/custom/flash-window.js
@@ -2,7 +2,7 @@ document.addEventListener('turbo:load', () => {
     const flashWindow = document.getElementsByClassName("flash-window")[0];
     if (flashWindow) {
         flashWindowToggle();
-        setTimeout(flashWindowToggle, 2000)
+        setTimeout(flashWindowToggle, 3000)
         // document.getElementsByClassName("flash-window-delete")[0].addEventListener('click', flashWindowToggle);
     }
     function flashWindowToggle()
