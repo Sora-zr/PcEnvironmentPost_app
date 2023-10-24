64ecc90dc99995844715ad3bb781695ac71f8d24
sora <h.sora1105@gmail.com>
2023-10-24 17:26:59 +0900

    Add: 画像投稿プレビューを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,17 @@ class Post < ApplicationRecord
   validates_presence_of :description
   validates :images, content_type: { in: %w[image/jpeg image/gif image/png image/jpg], message: "有効なフォーマットではありません。" },
             size: { less_than: 5.megabytes, message: " 5MBを超える画像はアップロードできません" }
+  validate :validate_image_count
 
   validates_presence_of :images
+
+  private
+
+  # imagesの枚数制限のためのバリデーション
+  def validate_image_count
+    if images.attached? && images.count > 4
+      errors.add(:images, "は最大4枚までです。")
+      images.purge
+    end
+  end
 end
